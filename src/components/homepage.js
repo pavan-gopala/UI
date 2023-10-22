91a784e8a867e6557146124f32c53734e4ef8b64
pavan2828 <pavan.gopala@securin.io>
2023-10-22 18:19:32 +0530

    update

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Navbar } from './Pages/MainPages/Navbar'
 import { Description } from './Pages/MainPages/Description'
+import { useEffect } from 'react'
+import mixpanel from 'mixpanel-browser'
 
 
 export const Homepage = () => {
+
+ 
+  useEffect(() => {
+    mixpanel.init('43bbf336f889d2d66414c8ff11e6e6fa', { debug: true, track_pageview: true, persistence: 'localStorage' });
+  }, []);
+ 
+
   return (
     <div style={{width:'100%'}}><Navbar/>
     <Description/>
