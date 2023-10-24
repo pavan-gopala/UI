a453f3c100bf1a53be11acd2abb65e656e8c6963
pavan2828 <pavan.gopala@securin.io>
2023-10-24 15:21:35 +0530

    update

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import { Navbar } from './Pages/MainPages/Navbar'
 import { Description } from './Pages/MainPages/Description'
-import { useTracking } from 'react-tracking'
-import { initGA, logPageView } from '../googleanalytics'
-import { useEffect } from 'react'
+
 
 export const Homepage = () => {
-    
-  useEffect(() => {
-    initGA();
-    logPageView();
-  }, []);
-
-  const { trackEvent } = useTracking();
 
   return (
     <div style={{width:'100%'}}><Navbar/>
