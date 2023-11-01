c946557417039a4ad507a2d8a9eeb0f515cd9c3f
pavan2828 <pavan.gopala@securin.io>
2023-11-01 11:47:01 +0530

    loader update

--- src/components/Pages/subpages/Loader.js
+++ src/components/Pages/subpages/Loader.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
 import { IconButton } from '@mui/material';
+import { SyncLoader } from 'react-spinners';
+import { useSelector } from 'react-redux';
+import { useTheme } from '@emotion/react';
+import '../../../styles/styles.css';
 
 export const Loader = () => {
-  return (
-    <IconButton sx={{padding:7}}><CircularProgress/></IconButton>
+
+  return (<div className="loader-container">
+  <div className="loadingio-spinner-gear-y4ot2tj9qw">
+    <div class="ldio-4xbc3nc6sx2">
+        <div>
+            <div></div><div></div><div></div><div></div><div></div><div></div>
+        </div>
+    </div>
+</div>
+ 
+</div>      
   )
 }
