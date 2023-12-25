eda0dee52ee0b333b3909c4b2ded7ad732cd04a7
pavan2828 <pavan.gopala@securin.io>
2023-12-25 17:16:21 +0530

    update

--- src/components/Pages/subpages/websiteperformancecheck/charts/performance.bar.js
+++ src/components/Pages/subpages/websiteperformancecheck/charts/performance.bar.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import '../../../../../styles/performancebar.css';
 import { useSelector } from 'react-redux';
-import { Grid, Hidden, IconButton, Typography, containerClasses } from '@mui/material';
+import { Grid, Hidden, IconButton, TableRow,TableContainer,TableCell, TableBody, TableHead,  Typography, containerClasses, Paper } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
@@ -11,27 +11,47 @@ import { light } from '@mui/material/styles/createPalette';
 import GradeIcon from '@mui/icons-material/Grade';
 import {ReactComponent as Gradesvg}  from '../../../../../Images/websiteperformancecheck/best.svg';
 import {ReactComponent as Categsvg} from '../../../../../Images/websiteperformancecheck/categorise.svg';
+import {ReactComponent as Dislikesvg} from '../../../../../Images/websiteperformancecheck/dislike.svg';
+import {ReactComponent as Shieldsvg} from '../../../../../Images/websiteperformancecheck/shield (1).svg';
+import {ReactComponent as Averagesvg} from '../../../../../Images/websiteperformancecheck/check.svg';
+import { TableCellComponent, TableHeaderCellComponent } from '../modifycelldata/ModifyCellData';
 
+import {
+  Grid as DataGrid,
+  Table,
+  TableHeaderRow,
+  TableColumnResizing
+} from '@devexpress/dx-react-grid-material-ui';
+import { TableRowCustom } from '../modifycelldata/ModifyCellData';
 export const PerformanceBar = () => {
   const  data = useSelector((state)=>state.mailvalidation?.validationResult?.data?state.mailvalidation.validationResult.data : '')
   const domain = useSelector((state)=>state.mailvalidation?.email)
   const progressData = data.necessaryData?.scores?? "";
-
-  console.log(progressData)
+  const [columnWidths, setColumnWidths] = useState([
+    {columnName: '', width:150}
+  ])
+ 
+  console.log("columnWidths:",columnWidths)
   const container = {
        GridClass :{
            alignItems: 'center',
            justifyContent: 'center',
-           margin: 'auto',
-           padding:'10px'
+           marginTop: '20px',
+           border:'1px solid orange',
+           padding:'8px',
+           marginLeft:'8px',
+           marginRight:'8px'
+           
        },
        GridClass1 :{
         alignItems: 'center',
         justifyContent: 'center',
-        margin: 'auto',
         border:'1px dotted rgb(234, 81, 65)',
-        padding:'30px',
-        margin:'40px',
+        padding:'25px',
+        margin:'20px',
+
+        
+        
        },
        font: {
          fontSize:'smaller',
@@ -44,6 +64,11 @@ export const PerformanceBar = () => {
          border: '1px solid rgb(234, 81, 65)',
          padding:'5px',
          marginBottom:'10px',
+       },
+       Icons :{
+        height:'4vh',
+        width:'4vh',
+        padding:'1px'
        }
       
   }
@@ -52,7 +77,7 @@ export const PerformanceBar = () => {
   if (score < 30) {
     gradient = `conic-gradient(#ff0000 ${score}%, #ff0000 0%, #ffff ${score}%)`; // red
   } else if (score <= 70) {
-    gradient = `conic-gradient(#ffae42 ${score}%, #ffae42 0%, #ffff ${score}%)`; // orange
+    gradient = `conic-gradient(#3568c5 ${score}%, #ffae42 0%, #ffff ${score}%)`; // orange
   } else {
     gradient = `conic-gradient(#008000 ${score}%, #008000 0%, #ffff ${score}%)`; // green
   } }
@@ -71,20 +96,22 @@ export const PerformanceBar = () => {
     if(Grade === 'A'){
       return ('#008000')
     }else if(Grade === 'B'){
-      return ('#ffae42')
+      return ('#3568c5')
     }else{
       return ('#ff0000')
     }
   }
 
+  
+ let index = 0;
  
   return (
-    <Grid  container width={"100%"} style={container.GridClass1}>
+    <Grid  container width={"100%"}  style={{...container.GridClass1 ,}} >
       <Grid className='hidemdup' item xs={12} style={{marginRight:'0%',marginBottom:'20px'}}>
       <h3 style={{color:'rgb(234, 81, 65)'}}>DETAILED REPORT:</h3>
       </Grid>
       
-      <Grid className='hidemddown' item xs={12} style={{marginRight:'80%',marginBottom:'20px'}}>
+      <Grid className='hidemddown ' item xs={12} style={{marginRight:'80%',marginBottom:'20px'}}>
       <h3 style={{color:'rgb(234, 81, 65)'}}>DETAILED REPORT:</h3>
       </Grid>
              {Object.entries(progressData).map(([key, value])=>{
@@ -104,33 +131,108 @@ export const PerformanceBar = () => {
                   margin:'auto'
                 }
               }
+             
+              
+              const Grade = assignGrade(score);
+              let gradecolor = assignGradeColor(Grade)
               let categorizeScore = (score) => {
                 if (score < 30) {
-                  return {categ:'Bad', Icon:<ClearIcon/>};
+                  return {categ:'Bad', Icon:<Dislikesvg style={{fill:gradecolor,...container.Icons}}/>};
                 } else if (score <= 70) {
-                  return {categ:'Average', Icon:<CheckIcon/>};
+                  return {categ:'Average', Icon:<Averagesvg style={{fill:gradecolor,...container.Icons}}/>};
                 } else {
-                  return {categ:'Good',Icon:<GppGoodIcon/>};
+                  return {categ:'Good',Icon:<GppGoodIcon style={{color:gradecolor,...container.Icons}}/>};
                 }
               }
               const categScore = categorizeScore(score);
-              const Grade = assignGrade(score);
-              let gradecolor = assignGradeColor(Grade)
+              let tableData = [
+
+                {
+                  icon: categScore.Icon,
+                  key: <p className='keyhead'>Score: </p>,
+                  score: <p className='valuehead'>{score}</p>,
+                },
+                {
+                  icon: <Categsvg style={{ fill:'rgb(189, 64, 137)',...container.Icons}}/>,
+                  key: <p className='keyhead'>Category: </p>,
+                  score: <p className='valuehead'>{categScore.categ}</p>,
+                },
+                {
+                  icon: <Gradesvg style={{fill:'rgb(3, 63, 99)',...container.Icons}}/>,
+                  key: <p className='keyhead'>Grade: </p>,
+                  score: <p className='valuehead' style={{color:gradecolor}}>{Grade}</p>,
+                },
+              ];
+
               
+              let columnName = "";
+              let columnsData = [{"name":'',"title":''}]
+              switch(true){
+                case key === "Performance":
+                  columnName = "performancescore"
+                  columnsData[0].name = columnName
+                  columnsData[0].title = <p className='TableHeaderDev'>{key}</p>
+                 
+                  break
+                case key === "Accessibility":
+                  columnName = "accessibility"
+                  columnsData[0].name = columnName
+                  columnsData[0].title = <p className='TableHeaderDev'>{key}</p>
+                 
+                  break;
+                case key === "Seo":
+                  columnName = "seo"
+                  columnsData[0].name = columnName
+                  columnsData[0].title = <p className='TableHeaderDev'>{key}</p>
+                 
+                  break;
+                case key === "BestPractices":
+                  columnName = "bestPratices"
+                  columnsData[0].name = columnName
+                  columnsData[0].title = <p className='TableHeaderDev'>{key}</p>
+                 
+                  break;
+                case key === 'Pwa':
+                  columnName = "pwa"
+                  columnsData[0].name = columnName
+                  columnsData[0].title = <p className ='TableHeaderDev'>{key}</p>
+                  
+                  break;
+              }
+
+               
+              
+            
+              
+              let rows = tableData.map((data) => ({
+                [columnName]: (
+                  <div className='inRow' style={{ margin: '2px', display:'flex', direction:'row',justifyContent:'flex-start', }}>
+             
+                    <div>{data.icon}</div>
+                    <div>{data.key}</div>
+                    <div>{data.score}</div>
+                  
+                  </div>
+                ),
+              }));
+               
               return (
-                    
-                      
-                            <Grid key={key} item xs={12} md={2} style={container.GridClass}>
-                            
-                              
-                        <div class="circle" style={performance.s1}>
+                            <Grid key={key} item xs={12} md={2} className='Detailitemcard' style={container.GridClass}>
+                            <div class="circle" style={performance.s1}>
                             <div class="inner">{score}</div>
-                        </div>
-                        <Grid className='ParentIconContainer' item xs={12} >
-                          <div className='IconContainer'><IconButton style={{color:gradecolor}}>{categScore.Icon}</IconButton><p className='keyhead'>{key}: </p><p className='valuehead'>{score}</p></div>
-                          <div className='IconContainer'><Categsvg style={{ fill:'rgb(189, 64, 137)',height:'4vh', width:'4vw'}}/><p className='keyhead'>CATEGORY: </p><p className='valuehead' >{categScore.categ}</p></div>
-                          <div className='IconContainer' ><Gradesvg style={{fill:gradecolor,height:'4vh', width:'4vw'}}/><p className='keyhead'>GRADE: </p><p className='valuehead' style={{color:gradecolor}}>{Grade}</p></div>
+                        </div> 
+                        <Grid className='ParentIconContainer' item  >
+
+                        <DataGrid
+                           rows={rows}
+                           columns={columnsData}
+                                    > 
+                        <Table rowComponent={TableRowCustom}  headerCellComponent={TableHeaderCellComponent}/>
+                        <TableColumnResizing  />
+                       <TableHeaderRow />
+                        </DataGrid>
                         </Grid>
+                        
                         
                         </Grid>
                    
---
+++ src/components/Pages/subpages/websiteperformancecheck/modifycelldata/ModifyCellData.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Table } from '@devexpress/dx-react-grid-material-ui';
+import { useState } from 'react';
+import '../../../../../styles/performancebar.css';
+
+
+const rowstyles = {
+    row:{
+        backgroundColor:'rgb(234, 81, 65,0.5)',
+        border:'1px solid green'
+    }
+}
+
+export function TableRowCustom(props) {
+    const [isHovered, setIsHovered] = useState(false);
+    
+    return (
+      <Table.Row
+        {...props}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className='TableRowDev'
+        style={{...rowstyles.row,  backgroundColor: isHovered ? 'white':'',marginBottom:'0px',borderBottom:'0px'   }}
+      />
+    );
+  }
+
+  export const TableCellComponent = (props) => {
+    return (
+      <Table.Cell
+        {...props}
+        style={{ backgroundColor: 'lightblue', color: 'black' }}
+      />
+    );
+  };
+
+  export const TableHeaderCellComponent = (props) => {
+    return (
+      <Table.HeaderCell
+        {...props}
+        style={{ backgroundColor: 'darkblue', color: 'white' }}
+      />
+    );
+  };
