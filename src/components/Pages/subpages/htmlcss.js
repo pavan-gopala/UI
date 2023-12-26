3b59645387b1a09eef81bf70469c00b8e2fe6964
pavan gopala <82314486+pavan-gopala@users.noreply.github.com>
2023-12-26 18:01:32 +0530

    Update subpages

---
+++ src/components/Pages/subpages/htmlcss.js
@@ -0,0 +1,484 @@
+<div id="growth-plan-box" class="shadow-lg bg-white rounded-3xl px-6 sm:px-8 py-4 sm:py-6 dark:bg-gray-800 ring-2 ring-indigo-600 dark:ring-indigo-300 snipcss-OzUDZ">
+  <div class="flex items-center justify-between gap-x-4">
+    <h3 class="text-lg font-semibold leading-8 text-indigo-600 dark:text-indigo-300">
+      Growth
+    </h3>
+    <div class="flex items-center justify-between gap-x-4">
+      <p id="highlight-pill" class="rounded-full bg-indigo-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-indigo-600 dark:text-indigo-300 dark:ring-1 dark:ring-indigo-300/50">
+        Recommended
+      </p>
+    </div>
+  </div>
+  <div>
+    <p class="mt-6 flex items-baseline gap-x-1">
+      <span id="growth-price-tag-amount" class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
+        €9
+      </span>
+      <span id="growth-price-tag-interval" class="text-sm font-semibold leading-6 text-gray-600 dark:text-gray-500">
+        /month
+      </span>
+    </p>
+    <p class="mt-1 text-xs">
+      + VAT if applicable
+    </p>
+    <button id="growth-checkout" onclick="if (true) {Paddle.Checkout.open({&quot;product&quot;:&quot;857097&quot;,&quot;success&quot;:&quot;/billing/upgrade-success&quot;,&quot;email&quot;:&quot;pavangopala793@gmail.com&quot;,&quot;theme&quot;:&quot;none&quot;,&quot;disableLogout&quot;:true,&quot;passthrough&quot;:75290})}" class="w-full mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 text-white bg-indigo-600 hover:bg-indigo-500">
+      Upgrade
+    </button>
+  </div>
+  <ul role="list" class="mt-8 space-y-3 text-sm leading-6 xl:mt-10 text-gray-600 dark:text-gray-100">
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      Up to 3 team members
+    </li>
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      Up to 10 sites
+    </li>
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      3 years of data retention
+    </li>
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      Intuitive, fast and privacy-friendly dashboard
+    </li>
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      Email/Slack reports
+    </li>
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      Google Analytics import
+    </li>
+    <li class="flex gap-x-3">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-5 text-indigo-600 dark:text-green-600" stroke="currentColor" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5">
+        </path>
+      </svg>
+      Goals and custom events
+    </li>
+  </ul>
+</div>
+
+
+  body {  
+    color:rgb(15 23 42/var(--tw-text-opacity));
+    line-height:inherit;
+    font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+  }  
+body { 
+/* CSS Variables that may have been missed get put on body */ 
+    --tw-ring-offset-width:  0px;  
+    --tw-ring-offset-color:  #fff;  
+    --tw-ring-color:  #3b82f680;  
+    --tw-ring-offset-shadow:  0 0 #0000;  
+    --tw-ring-shadow:  0 0 #0000;  
+    --tw-shadow:  0 0 #0000;  
+    --tw-text-opacity:  1;  
+    --tw-bg-opacity:  1;  
+    --tw-shadow:  0 10px 15px -3px #0000001a,0 4px 6px -4px #0000001a;  
+    --tw-shadow-colored:  0 10px 15px -3px var(--tw-shadow-color),0 4px 6px -4px var(--tw-shadow-color); 
+    --tw-ring-offset-shadow:  var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);  
+    --tw-ring-shadow:  var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);  
+    --tw-ring-opacity:  1;  
+    --tw-ring-color:  rgb(79 70 229/var(--tw-ring-opacity)); 
+    --tw-ring-offset-width:  0px;  
+    --tw-ring-offset-color:  #fff;  
+    --tw-ring-color:  #3b82f680;  
+    --tw-ring-offset-shadow:  0 0 #0000;  
+    --tw-ring-shadow:  0 0 #0000;  
+    --tw-shadow:  0 0 #0000;  
+    --tw-text-opacity:  1;  
+    --tw-text-opacity:  1;  
+    --tw-bg-opacity:  1;  
+    --tw-text-opacity:  1;  
+    --tw-bg-opacity:  1;  
+    --tw-space-y-reverse:  0;  
+} 
+
+* { 
+    border: 0 solid #e2e8f0; 
+    box-sizing: border-box;
+} 
+
+* { 
+    --tw-ring-offset-width: 0px; 
+    --tw-ring-offset-color: #fff; 
+    --tw-ring-color: #3b82f680; 
+    --tw-ring-offset-shadow: 0 0 #0000; 
+    --tw-ring-shadow: 0 0 #0000; 
+    --tw-shadow: 0 0 #0000; 
+} 
+
+.mt-6 { 
+    margin-top: 1.5rem;
+} 
+
+@media (min-width: 1024px){ 
+  .lg\:mx-0 { 
+    margin-left: 0; 
+    margin-right: 0;
+  } 
+
+  .lg\:max-w-none { 
+    max-width: none;
+  } 
+
+  .lg\:grid-cols-3 { 
+    grid-template-columns: repeat(3,minmax(0,1fr));
+  } 
+}     
+
+.px-6 { 
+    padding-left: 1.5rem; 
+    padding-right: 1.5rem;
+} 
+
+@media (min-width: 1024px){ 
+  .lg\:px-20 { 
+    padding-left: 5rem; 
+    padding-right: 5rem;
+  } 
+}     
+
+.text-gray-900 { 
+    --tw-text-opacity: 1; 
+    color: rgb(15 23 42/var(--tw-text-opacity));
+} 
+
+@media (min-width: 640px){ 
+  .sm\:pb-16 { 
+    padding-bottom: 4rem;
+  } 
+}     
+
+body { 
+    line-height: inherit; 
+    margin: 0;
+} 
+
+body { 
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale; 
+    overflow-x: hidden; 
+    width: 100vw;
+} 
+
+.flex { 
+    display: flex;
+} 
+
+html { 
+    -webkit-text-size-adjust: 100%; 
+    font-feature-settings: normal; 
+    font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji; 
+    font-variation-settings: normal; 
+    line-height: 1.5; 
+    -moz-tab-size: 4; 
+    -o-tab-size: 4; 
+    tab-size: 4;
+} 
+
+html { 
+    --tw-text-opacity: 1; 
+    color: rgb(30 41 59/var(--tw-text-opacity));
+} 
+
+.rounded-3xl { 
+    border-radius: 1.5rem;
+} 
+
+.bg-white { 
+    --tw-bg-opacity: 1; 
+    background-color: rgb(255 255 255/var(--tw-bg-opacity));
+} 
+
+.py-4 { 
+    padding-bottom: 1rem; 
+    padding-top: 1rem;
+} 
+
+.shadow-lg { 
+    box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000),var(--tw-shadow);
+} 
+
+.shadow-lg { 
+    --tw-shadow: 0 10px 15px -3px #0000001a,0 4px 6px -4px #0000001a; 
+    --tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color),0 4px 6px -4px var(--tw-shadow-color);
+} 
+
+.ring-2 { 
+    --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color); 
+    --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color); 
+    box-shadow: var(--tw-ring-offset-shadow),var(--tw-ring-shadow),var(--tw-shadow,0 0 #0000);
+} 
+
+.ring-indigo-600 { 
+    --tw-ring-opacity: 1; 
+    --tw-ring-color: rgb(79 70 229/var(--tw-ring-opacity));
+} 
+
+@media (min-width: 640px){ 
+  .sm\:px-8 { 
+    padding-left: 2rem; 
+    padding-right: 2rem;
+  } 
+
+  .sm\:py-6 { 
+    padding-bottom: 1.5rem; 
+    padding-top: 1.5rem;
+  } 
+}     
+
+*,:after,:before { 
+    border: 0 solid #e2e8f0; 
+    box-sizing: border-box;
+} 
+
+*,:backdrop,:after,:before { 
+    --tw-ring-offset-width: 0px; 
+    --tw-ring-offset-color: #fff; 
+    --tw-ring-color: #3b82f680; 
+    --tw-ring-offset-shadow: 0 0 #0000; 
+    --tw-ring-shadow: 0 0 #0000; 
+    --tw-shadow: 0 0 #0000; 
+} 
+
+.items-center { 
+    align-items: center;
+} 
+
+.justify-between { 
+    justify-content: space-between;
+} 
+
+.gap-x-4 { 
+    -moz-column-gap: 1rem; 
+    column-gap: 1rem;
+} 
+
+ul { 
+    list-style: none; 
+    margin: 0; 
+    padding: 0;
+} 
+
+.mt-8 { 
+    margin-top: 2rem;
+} 
+
+.text-sm { 
+    font-size: .875rem; 
+    line-height: 1.25rem;
+} 
+
+.leading-6 { 
+    line-height: 1.5rem;
+} 
+
+.text-gray-600 { 
+    --tw-text-opacity: 1; 
+    color: rgb(71 85 105/var(--tw-text-opacity));
+} 
+
+@media (min-width: 1280px){ 
+  .xl\:mt-10 { 
+    margin-top: 2.5rem;
+  } 
+}     
+
+h3 { 
+    font-size: inherit; 
+    font-weight: inherit;
+} 
+
+h3 { 
+    margin: 0;
+} 
+
+.text-lg { 
+    font-size: 1.125rem; 
+    line-height: 1.75rem;
+} 
+
+.font-semibold { 
+    font-weight: 600;
+} 
+
+.leading-8 { 
+    line-height: 2rem;
+} 
+
+.text-indigo-600 { 
+    --tw-text-opacity: 1; 
+    color: rgb(79 70 229/var(--tw-text-opacity));
+} 
+
+p { 
+    margin: 0;
+} 
+
+.items-baseline { 
+    align-items: baseline;
+} 
+
+.gap-x-1 { 
+    -moz-column-gap: .25rem; 
+    column-gap: .25rem;
+} 
+
+.mt-1 { 
+    margin-top: .25rem;
+} 
+
+.text-xs { 
+    font-size: .75rem; 
+    line-height: 1rem;
+} 
+
+button { 
+    font-feature-settings: inherit; 
+    color: inherit; 
+    font-family: inherit; 
+    font-size: 100%; 
+    font-variation-settings: inherit; 
+    font-weight: inherit; 
+    line-height: inherit; 
+    margin: 0; 
+    padding: 0;
+} 
+
+button { 
+    text-transform: none;
+} 
+
+button { 
+    -webkit-appearance: button; 
+    background-color: initial; 
+    background-image: none;
+} 
+
+button { 
+    cursor: pointer;
+} 
+
+.block { 
+    display: block;
+} 
+
+.w-full { 
+    width: 100%;
+} 
+
+.rounded-md { 
+    border-radius: .375rem;
+} 
+
+.bg-indigo-600 { 
+    --tw-bg-opacity: 1; 
+    background-color: rgb(79 70 229/var(--tw-bg-opacity));
+} 
+
+.px-3 { 
+    padding-left: .75rem; 
+    padding-right: .75rem;
+} 
+
+.py-2 { 
+    padding-bottom: .5rem; 
+    padding-top: .5rem;
+} 
+
+.text-center { 
+    text-align: center;
+} 
+
+.text-white { 
+    --tw-text-opacity: 1; 
+    color: rgb(255 255 255/var(--tw-text-opacity));
+} 
+
+.hover\:bg-indigo-500:hover { 
+    --tw-bg-opacity: 1; 
+    background-color: rgb(99 102 241/var(--tw-bg-opacity));
+} 
+
+.gap-x-3 { 
+    -moz-column-gap: .75rem; 
+    column-gap: .75rem;
+} 
+
+.space-y-3 > :not([hidden]) ~ :not([hidden])  { 
+    --tw-space-y-reverse: 0; 
+    margin-bottom: calc(.75rem*var(--tw-space-y-reverse)); 
+    margin-top: calc(.75rem*(1 - var(--tw-space-y-reverse)));
+} 
+
+.rounded-full { 
+    border-radius: 9999px;
+} 
+
+.bg-indigo-600\/10 { 
+    background-color: #4f46e51a;
+} 
+
+.px-2\.5 { 
+    padding-left: .625rem; 
+    padding-right: .625rem;
+} 
+
+.py-1 { 
+    padding-bottom: .25rem; 
+    padding-top: .25rem;
+} 
+
+.leading-5 { 
+    line-height: 1.25rem;
+} 
+
+.text-4xl { 
+    font-size: 2.25rem; 
+    line-height: 2.5rem;
+} 
+
+.font-bold { 
+    font-weight: 700;
+} 
+
+.tracking-tight { 
+    letter-spacing: -.025em;
+} 
+
+svg { 
+    display: block; 
+    vertical-align: middle;
+} 
+
+.h-6 { 
+    height: 1.5rem;
+} 
+
+.w-5 { 
+    width: 1.25rem;
+} 
+
+
+
